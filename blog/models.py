9a0a70dee7e40e16f76408853fddb5795783021c
sandsot <sandel21@naver.com>
2022-10-26 12:18:38 +0900

    1026 오전 commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from email import contentmanager
+from turtle import title
+from unittest.util import _MAX_LENGTH
+from venv import create
 from django.db import models
 
-# Create your models here.
+# Post Model
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    create_at = models.DateTimeField()
+
