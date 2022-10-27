4db94d2a1a0ec77555d62ba834dd3d12fdca8f73
sandsot <sandel21@naver.com>
2022-10-27 11:58:05 +0900

    Absolute add

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,12 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    create_at = models.DateTimeField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        # TODO : 장고의 URL Reverse 기능을 사용하기
+        return f"/blog/{self.pk}/"
+
+    def __str__(self):
+        return f"[{self.pk}] {self.title}"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    post_qs = Post.objects.all().order_by('-pk')
+
+    return render(request, 'blog/index.html',{'post_list':post_qs,})
+
+def post_detail(request):
+
+    post = Post.objects.get(pk=pk)
+    return 
